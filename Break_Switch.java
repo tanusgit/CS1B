4ae4622ad80b33016e596f1c220d1f20bed504d3
Tanu <24976766+tanusgit@users.noreply.github.com>
2018-10-14 12:36:30 -0700

    Create Break_Switch.java

---
+++ Break_Switch.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{//break;
+		// prints 1 to 10 except 5
+		int n =1;
+		switch(n){
+			case(1): System.out.print("one");
+			break;
+			case(8): System.out.print("tew");
+			break;
+			case(3): System.out.print("three");
+			break;
+    }
+	}
+}
